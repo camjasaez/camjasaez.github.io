64b51336dbb4055e85deeae05ec3b3b99b731fc6
Camilo Sáez Garrido <camjasaez@gmail.com>
2024-03-21 01:06:06 -0300

    Avances version beta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://camjasaez.github.io',
+  integrations: [react(), tailwind({ applyBaseStyles: false })],
 });
